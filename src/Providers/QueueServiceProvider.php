1affdb3d1c922ae912afa664f84ed7d391e8a8b3
kooriv <kooroshsafeashrafi@gmail.com>
2023-05-27 18:57:44 +0330

    fixed serviceProvider

--- src/Providers/QueueServiceProvider.php
+++ src/Providers/QueueServiceProvider.php
@@ -8,9 +8,9 @@ use Kooriv\Queue\Illuminate\Queue\Console\IdentifierTableCommand;
 
 class QueueServiceProvider extends ServiceProvider
 {
-    public function register()
+    public function boot(): void
     {
-        /** @var QueueManager $queue */
+    	/** @var QueueManager $queue */
         $queue = $this->app['queue'];
         $queue->addConnector('identifier-database', function () {
             return new IdentifierDatabaseConnector($this->app['db']);
@@ -19,10 +19,7 @@ class QueueServiceProvider extends ServiceProvider
         if ($this->app->runningInConsole()) {
             $this->commands(commands: [IdentifierTableCommand::class]);
         }
-    }
 
-    public function boot(): void
-    {
         $this->mergeConfigFrom(path: dirname(__DIR__, 2) . '/config/identifier-database.php', key: 'queue.connections.identifier-database');
         if ($this->isLumen()) {
             $this->app->configure(name: 'queue.connections.identifier-database');
